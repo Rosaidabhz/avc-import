2ad3623219cc8fb2f79bc878a98689d01b1383ca
rosaidabhz <rosaidabhz@gmail.com>
2023-03-31 17:27:43 -0500

    changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,16 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, AfterViewInit,  HostListener } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
-import { ProductosService } from 'src/app/services/productos.service';
 import { WhatsappService } from 'src/app/services/whatsapp.service';
 import { CartItem } from 'src/app/Shared/interfaces/CartItem';
+
+declare var M: any;
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
   isSticky: boolean = false;
   cart: CartItem[] = [];
   totalCart = 0;
@@ -47,83 +48,24 @@ export class HeaderComponent {
     this.shoppingCartFlat = !this.shoppingCartFlat;
   }
    
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    const elemssidenav = document.querySelectorAll('.sidenav');
+    const instancessidenav = M.Sidenav.init(elemssidenav, { edge: 'left' });
     
-
-    // constants
-  const body = document.querySelector("body"),
-  loader = document.querySelector(".loader-wrap"),
-  links = document.querySelectorAll('a[href="#"]'),
-  nav = document.querySelector("header nav"),
-  navToggle = document.querySelector("header nav .toggle"),
-  navSpanMiddle = document.querySelector("header nav .toggle .middle"),
-  navNavigationBar = document.querySelector("header nav .navigation-bar"),
-  navNavigationBarLi = document.querySelectorAll(
-      "header nav .navigation-bar li"
-  ),
-  headerText = document.querySelector("header .text"),
-  headerSection = document.querySelector("header"),
-  aboutSection = document.querySelector(".about-us"),
-  recipeSection = document.querySelector(".recipes"),
-  menuSection = document.querySelector(".menu"),
-  fixedImageSection = document.querySelector(".fixed-image"),
-  footerSection = document.querySelector("footer"),
-  dotOne = document.querySelector(".dots .one"),
-  dotTwo = document.querySelector(".dots .two"),
-  dotThree = document.querySelector(".dots .three"),
-  dots = document.querySelectorAll(".dots > div"),
-  logoImage = document.querySelector("header nav .logo img"),
-  svgDown = document.querySelector("header .arrow-down"),
-  svgUp = document.querySelector(".copyright .arrow-up"),
-  menuImgs = document.querySelectorAll(".menu .menu-image-container img"),
-  boxModel = document.querySelector(".menu .box-model"),
-  menuImageContainer = document.querySelector(".menu-image-container"),
-  boxModelArrow = document.querySelector(".menu .box-model .arrow"),
-  boxModelImage = document.querySelector(".menu .box-model img"),
-  pageTitle = document.querySelector("title");
+  }    
   
-  
-  // toggle hamburger menu button
-  navToggle!.addEventListener("click", () => {
-  navToggle!.classList.toggle("active");
-  navSpanMiddle!.classList.toggle("hide");
-  navNavigationBar!.classList.toggle("show");
-  });
-  
-  // show active navigationbar li
-  navNavigationBarLi.forEach(li =>
-  li.addEventListener("click", () => {
-      const arr = Array.from(li.parentElement!.children);
-      arr.forEach(li => li.classList.remove("active"));
-      li.classList.add("active");
-  })
-  );
-  
-  // svg-up smooth scroll
-  svgUp!.addEventListener("click", () => {
-  window.scroll({
-      top: 0,
-      behavior: "smooth"
-  });
-  });
-  
-  window.onscroll = function() {
-  // header welcome fade out and in
-
-  }   
-
-}
   sendWhatsapp() {
-      let message = '';
-      this.cart.find(product => {
+    let message = '';
+    this.cart.find(product => {
       message += `: ${product.title}, Cantidad-${product.quantity}, Precio: ${product.price}  \n`;
-       });
- 
-        message += `Total: ${this.total()} dolares`;
- 
- 
-        const url = this.wpService.contact(message);
-        console.log(url);
-        window.open(url, '_blank');
-
-        this.wpService.contact(message)}}
+    });
+   
+    message += `Total: ${this.total()} dolares`;
+   
+    const url = this.wpService.contact(message);
+    console.log(url);
+    window.open(url, '_blank');
+  
+    this.wpService.contact(message);
+  }
+}
