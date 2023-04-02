681efaf855e6fc89b2c2865b29a85773968bf28c
rosaidabhz <rosaidabhz@gmail.com>
2023-04-02 18:09:14 -0500

    changes ruta img

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -11,56 +11,56 @@ export class ProductosService {
       title: 'Maquina manual',
       caption: 'COD: SB04A',
       price: 120,
-      img: '/assets/product_images/1.png'
+      img: '1.png'
     },
     {
       id: 2,
       title: 'Maquina Manual',
       caption: 'COD: ST-130',
       price: 140,
-      img: '/assets/product_images/2.png'
+      img: '2.png'
     },
     {
       id: 3,
       title: 'Maquina Doble Estacion',
       caption: 'COD: ST-210',
       price: 170,
-      img: '/assets/product_images/3.png'
+      img: '3.png'
     },
     {
       id: 4,
       title: 'Maquina Multifuncional 8-1',
       caption: 'COD: ST-220',
       price:350,
-      img: '/assets/product_images/4.png'
+      img: '4.png'
     },
     {
       id: 5,
       title: 'Maquina Multifuncional 8-1',
       caption: 'COD: ST-220',
       price: 390,
-      img: '/assets/product_images/5.png'
+      img: '5.png'
     },
     {
       id: 6,
       title: 'Maquina Multifuncional 11-1',
       caption: 'COD: P8200-V11',
       price: 490,
-      img: '/assets/product_images/6.png'
+      img: '6.png'
     },
     {
       id: 7,
       title: 'Maquina Multifuncional 5-1',
       caption: 'COD: SB04W',
       price: 420,
-      img: '/assets/product_images/7.png'
+      img: '7.png'
     },
     {
       id: 8,
       title: 'Maquina de Gorras 2-1',
       caption: 'Resistencia cuadrada',
       price: 260,
-      img: '/assets/product_images/8.png'
+      img: '8.png'
     },
   ];
 
