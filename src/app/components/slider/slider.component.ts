272ac5004fbb8d73aaf87d415c5c3700982c113f
rosaidabhz <rosaidabhz@gmail.com>
2023-04-07 14:11:57 -0500

    slider

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -5,55 +5,71 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
-export class SliderComponent implements OnInit {
-  private intervalo: any; 
+export class SliderComponent implements OnInit{
 
+   
   ngOnInit(): void {
-    var botonDerecho = document.getElementsByClassName('icono-derecho')[0];  
-    var botonIzquierdo = document.getElementsByClassName('icono-izquierdo')[0]; 
+
+
+
+  // ------------ Recogiendo variables ---------------- // 
  
-    var slider = document.getElementsByClassName('slider')[0] as HTMLElement;
+var botonDerecho = document.getElementsByClassName('icono-derecho')[0]  
+var botonIzquierdo = document.getElementsByClassName('icono-izquierdo')[0] 
+ 
+var slider = document.getElementsByClassName('slider')[0]  as HTMLElement
 
-    var sliders = document.getElementsByClassName('img-slider').length; 
-    var contador = 0; 
-
-    const moverDerecha = () => { 
-      contador++;
-      if (contador > sliders - 1) { 
-        contador = 0; 
-        slider.style.transition = 'none'; 
-        slider.style.marginLeft = '0%'; 
-        setTimeout(() => {
-          slider.style.transition = 'margin-left 0.5s ease';
-        }, 100);
-      } else {
-        slider.style.marginLeft = `-${contador * 100}%`;
-      }
-    
-      clearInterval(this.intervalo);
-    
-      this.intervalo = setInterval(moverDerecha, 6000);
-    }
-    
-    
-    
-    
-    const moverIzquierda = () => { 
-      contador--;
-      if (contador < 0) { 
-        contador = sliders - 1; 
-      }
-       
-      slider.style.marginLeft = `-${contador * 100}%`;
+var sliders = document.getElementsByClassName('img-slider').length 
+var contador = 0; // ---- Se define un contador que comience en 0 ---- // 
+ 
+ 
+// -------------- Declarar Funcion Derecha ----------------- // 
+ 
+function moverDerecha(){ 
+    contador++; // ---- Por cada vez que oprima el boton el contador pasa de 0 a 1 ----// 
+     
+    // ---- Condicion para devolverse cuando llegue a su fin // 
+ 
+    if(contador > sliders - 1){ 
+        contador = 0 
+    } 
+ 
+    // Por cada vez que de click hara un margin left ocultandolo pero mostrando la siguiente // 
+     
+    slider.style.marginLeft = `-${contador * 100}%`
   
-      clearInterval(this.intervalo);
-  
-      this.intervalo = setInterval(moverDerecha, 6000);
-    }
-
-    this.intervalo = setInterval(moverDerecha, 6000); 
-
-    botonDerecho.addEventListener('click', moverDerecha); 
-    botonIzquierdo.addEventListener('click', moverIzquierda);
-  }
-}
+ 
+    // Funcion para que se repita el tiempo cuando cambie // 
+ 
+    clearInterval(intervalo) 
+    intervalo = setInterval(moverDerecha, 6000) 
+} 
+ 
+// ------- SLIDER AUTOMATICO ------- //  
+ 
+var intervalo = setInterval(moverDerecha, 6000) 
+ 
+// -------------- Declarar Funcion izquierda ----------------- // 
+ 
+function moverIzquierda(){ 
+    contador--; // ---- Por cada vez que oprima el boton el contador bajara un numero ----// 
+     
+    // ---- Condicion para devolverse cuando llegue al comienzo // 
+ 
+    if(contador < 0){ 
+        contador = sliders -1; 
+    } 
+ 
+    // Por cada vez que de click hara un margin left ocultandolo pero mostrando la siguiente // 
+     
+    slider.style.marginLeft = `-${contador * 100}%`  
+ 
+    // Funcion para que se repita el tiempo cuando cambie // 
+ 
+    clearInterval(intervalo) 
+    intervalo = setInterval(moverDerecha, 6000) 
+} 
+ 
+botonDerecho.addEventListener('click', moverDerecha) 
+botonIzquierdo.addEventListener('click', moverIzquierda)
+}}
